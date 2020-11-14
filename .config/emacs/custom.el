d74980d9a737a2815e76a1b37af76296b03d1611
elxbarbosa <elxbarbosa@gmail.com>
2020-11-14 00:28:19 -0300

    mx: add custom file

---
+++ .config/emacs/custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(exec-path-from-shell php-mode rust-mode csharp-mode binder prettier-js rake eshell-did-you-mean eshell-syntax-highlighting eshell-toggle litable isolate frog-jump-buffer dockerfile-mode cask commander git shut-up package-build ansi languagetool luarocks define-word zoom diff-hl multiple-cursors iedit grip-mode ido-completing-read+ eyebrowse paradox crux flymake-diagnostic-at-point flymake-shellcheck webpaste imgbb doom-modeline nov olivetti smartparens langtool dired-git-info dired-collapse bug-hunter google-translate anzu esh-help esh-autosuggest eshell-prompt-extras aggressive-indent company-quickhelp company-statistics markdown-toc htmlize yasnippet-snippets goto-chg gif-screencast org-bullets elfeed erc-image erc-hl-nicks rainbow-delimiters fill-column-indicator expand-region which-key beacon flx-ido ido-hacks ido-at-point all-the-icons-dired peep-dired dired-ranger dired-sidebar writeroom-mode wgrep gitattributes-mode gitignore-mode flymake-json dap-mode rainbow-mode hl-todo indent-guide gnu-elpa-keyring-update fish-mode json-mode outshine company-shell highlight-indent-guides helpful emmet-mode org-present fish-completion deadgrep helm outline-toc ox-epub vterm company-box yaml-mode editorconfig typescript-mode basic-ide hgignore-mode 0blayout doom-themes emms magit bufler highlight-symbol slime-company darkroom org-journal lsp-ui lua-mode org-make-toc buffer-expose git-link rvm rbenv chruby ruby-test-mode rubocop robe rufo ruby-tools rubocopfmt rspec-mode shackle company-inf-ruby ruby-compilation bundler yard-mode yari selectrum-prescient counsel nix-mode nixos-options nixpkgs-fmt jetbrains-darcula-theme eglot undo-tree f s use-package))
+ '(safe-local-variable-values
+   '((whitespace-line-column . 80)
+     (ruby-compilation-executable . "ruby")
+     (ruby-compilation-executable . "ruby1.8")
+     (ruby-compilation-executable . "ruby1.9")
+     (ruby-compilation-executable . "rbx")
+     (ruby-compilation-executable . "jruby"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
