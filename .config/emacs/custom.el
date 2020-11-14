ba7cc1eb8a1c22f64f9186179f4c5cfd21125cd0
elxbarbosa <elxbarbosa@gmail.com>
2020-11-14 07:08:38 -0300

    mx: add exec-path-from-shell

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -3,15 +3,49 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#fafafa" "#e45649" "#50a14f" "#986801" "#4078f2" "#a626a4" "#0184bc" "#383a42"])
+ '(custom-safe-themes
+   '("e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" default))
+ '(fci-rule-color "#383a42")
+ '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#4078f2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#50a14f"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#9ca0a4"))
+ '(objed-cursor-color "#e45649")
  '(package-selected-packages
    '(exec-path-from-shell php-mode rust-mode csharp-mode binder prettier-js rake eshell-did-you-mean eshell-syntax-highlighting eshell-toggle litable isolate frog-jump-buffer dockerfile-mode cask commander git shut-up package-build ansi languagetool luarocks define-word zoom diff-hl multiple-cursors iedit grip-mode ido-completing-read+ eyebrowse paradox crux flymake-diagnostic-at-point flymake-shellcheck webpaste imgbb doom-modeline nov olivetti smartparens langtool dired-git-info dired-collapse bug-hunter google-translate anzu esh-help esh-autosuggest eshell-prompt-extras aggressive-indent company-quickhelp company-statistics markdown-toc htmlize yasnippet-snippets goto-chg gif-screencast org-bullets elfeed erc-image erc-hl-nicks rainbow-delimiters fill-column-indicator expand-region which-key beacon flx-ido ido-hacks ido-at-point all-the-icons-dired peep-dired dired-ranger dired-sidebar writeroom-mode wgrep gitattributes-mode gitignore-mode flymake-json dap-mode rainbow-mode hl-todo indent-guide gnu-elpa-keyring-update fish-mode json-mode outshine company-shell highlight-indent-guides helpful emmet-mode org-present fish-completion deadgrep helm outline-toc ox-epub vterm company-box yaml-mode editorconfig typescript-mode basic-ide hgignore-mode 0blayout doom-themes emms magit bufler highlight-symbol slime-company darkroom org-journal lsp-ui lua-mode org-make-toc buffer-expose git-link rvm rbenv chruby ruby-test-mode rubocop robe rufo ruby-tools rubocopfmt rspec-mode shackle company-inf-ruby ruby-compilation bundler yard-mode yari selectrum-prescient counsel nix-mode nixos-options nixpkgs-fmt jetbrains-darcula-theme eglot undo-tree f s use-package))
+ '(pdf-view-midnight-colors (cons "#383a42" "#fafafa"))
+ '(rustic-ansi-faces
+   ["#fafafa" "#e45649" "#50a14f" "#986801" "#4078f2" "#a626a4" "#0184bc" "#383a42"])
  '(safe-local-variable-values
    '((whitespace-line-column . 80)
      (ruby-compilation-executable . "ruby")
      (ruby-compilation-executable . "ruby1.8")
      (ruby-compilation-executable . "ruby1.9")
      (ruby-compilation-executable . "rbx")
-     (ruby-compilation-executable . "jruby"))))
+     (ruby-compilation-executable . "jruby")))
+ '(vc-annotate-background "#fafafa")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#50a14f")
+    (cons 40 "#688e35")
+    (cons 60 "#807b1b")
+    (cons 80 "#986801")
+    (cons 100 "#ae7118")
+    (cons 120 "#c37b30")
+    (cons 140 "#da8548")
+    (cons 160 "#c86566")
+    (cons 180 "#b74585")
+    (cons 200 "#a626a4")
+    (cons 220 "#ba3685")
+    (cons 240 "#cf4667")
+    (cons 260 "#e45649")
+    (cons 280 "#d2685f")
+    (cons 300 "#c07b76")
+    (cons 320 "#ae8d8d")
+    (cons 340 "#383a42")
+    (cons 360 "#383a42")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
