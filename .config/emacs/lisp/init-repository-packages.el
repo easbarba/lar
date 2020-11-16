f639481fb62dd7d08eed17efbdd42d18c14a5cb0
Euber Alexandre Barbosa <elxbarbosa@debian.debianD>
2020-11-16 18:04:03 -0500

    up

--- .config/emacs/lisp/init-repository-packages.el
+++ .config/emacs/lisp/init-repository-packages.el
@@ -3,26 +3,6 @@
 ;;; ------------------------------------------------------------
 ;;; * PACOTES EXTERNOS
 
-(defun e/install-external-packages (lst)
-  "Install LST list."
-  (interactive)
-
-  (package-refresh-contents)
-  (dolist (package lst)
-    (when (not (package-installed-p package))
-      (package-install package))))
-
-(let ((deps '(use-package s f dash)))
- (unless (package-installed-p 'use-package) ;; TODO check all deps
-  (package-refresh-contents)
-  (e/install-external-packages deps)))
-
-(when (require 'use-package nil :noerror)
-  (require 'use-package)
-  (setq-default use-package-verbose nil
-		use-package-expand-minimally t
-		use-package-enable-imenu-support t))
-
 ;; * LIBRARIES
 
 (use-package f)
