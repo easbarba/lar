3d6f21cc9c68577e0b8a5af53d543dbbd55774fa
elxbarbosa <elxbarbosa@gmail.com>
2020-11-17 07:14:53 -0300

    mx: up

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -2,19 +2,14 @@
 
 ;;; Description: GNU Emacs Configurations
 
-;; (when (version< emacs-version "27.1")
-;;   (load (concat user-emacs-directory "early-init.el"))
-
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 ;; SETTINGS
-
 (require 'init-base)
 (require 'init-settings)
 (require 'init-modeline)
 
 ;; BUILTINS
-
 (require 'init-builtin)
 (require 'init-dired)
 (require 'init-languages)
@@ -23,7 +18,6 @@
 (require 'init-my)
 
 ;; * LOAD EXTERNAL
-
 (require 'init-repository-get)
 (require 'init-repository-packages)
 (require 'init-builtin-extras)
