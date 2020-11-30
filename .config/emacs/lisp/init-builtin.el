945b7ece5d63be899d4e592055c1d9dadec3645b
Euber Alexandre Barbosa <elxbarbosa@debian.debianD>
2020-11-30 00:34:49 -0300

    erc add more roms

--- .config/emacs/lisp/init-builtin.el
+++ .config/emacs/lisp/init-builtin.el
@@ -473,7 +473,8 @@
 		   erc-interpret-mirc-color t
 		   erc-spelling-dictionaries '(("#emacs" "american"))
 		   erc-autojoin-channels-alist '(("freenode.net"
-						  "#rust"
+						  "#bootstrappable"
+						  "##c"
 						  "#emacs"))
 		   erc-lurker-hide-list '("JOIN" "PART" "QUIT")
 		   erc-track-exclude-types
