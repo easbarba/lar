d95b97df71086a28c87b66107833ede57475fe45
elxbarbosa <elxbarbosa@gmail.com>
2020-12-17 23:47:02 -0300

    mx#custom: clean up more

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -8,20 +8,11 @@
  '(custom-safe-themes
    '("7b3d184d2955990e4df1162aeff6bfb4e1c3e822368f0359e15e2974235d9fa8" "4f01c1df1d203787560a67c1b295423174fd49934deb5e6789abd1e61dba9552" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "6c9cbcdfd0e373dc30197c5059f79c25c07035ff5d0cc42aa045614d3919dab4" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "5b809c3eae60da2af8a8cfba4e9e04b4d608cb49584cb5998f6e4a1c87c057c4" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "d5a878172795c45441efcd84b20a14f553e7e96366a163f742b95d65a3f55d71" "74ba9ed7161a26bfe04580279b8cad163c00b802f54c574bfa5d924b99daa4b9" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "37a4701758378c93159ad6c7aceb19fd6fb523e044efe47f2116bc7398ce20c9" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" default))
  '(fci-rule-color "#383a42")
- '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#4078f2"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#50a14f"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#9ca0a4"))
- '(objed-cursor-color "#e45649")
  '(package-selected-packages
    '(neotree quelpa-use-package quelpa go-mode pdf-tools exec-path-from-shell rake eshell-did-you-mean eshell-syntax-highlighting eshell-toggle litable isolate frog-jump-buffer dockerfile-mode cask commander git package-build ansi define-word zoom diff-hl multiple-cursors iedit grip-mode ido-completing-read+ eyebrowse paradox crux flymake-diagnostic-at-point flymake-shellcheck webpaste imgbb doom-modeline nov olivetti smartparens dired-git-info dired-collapse bug-hunter google-translate anzu esh-help esh-autosuggest eshell-prompt-extras aggressive-indent company-quickhelp company-statistics markdown-toc htmlize yasnippet-snippets goto-chg gif-screencast org-bullets elfeed erc-image erc-hl-nicks rainbow-delimiters fill-column-indicator expand-region which-key beacon flx-ido ido-hacks ido-at-point all-the-icons-dired peep-dired dired-ranger dired-sidebar writeroom-mode wgrep gitattributes-mode gitignore-mode flymake-json dap-mode rainbow-mode hl-todo indent-guide gnu-elpa-keyring-update fish-mode json-mode outshine company-shell highlight-indent-guides helpful emmet-mode org-present fish-completion deadgrep helm outline-toc ox-epub company-box yaml-mode editorconfig hgignore-mode 0blayout doom-themes emms magit bufler highlight-symbol slime-company darkroom org-journal lsp-ui org-make-toc buffer-expose git-link rufo shackle yard-mode counsel eglot undo-tree f s use-package))
  '(pdf-view-midnight-colors (cons "#383a42" "#fafafa"))
  '(safe-local-variable-values
-   '((whitespace-line-column . 80)
-     (ruby-compilation-executable . "ruby")
-     (ruby-compilation-executable . "ruby1.8")
-     (ruby-compilation-executable . "ruby1.9")
-     (ruby-compilation-executable . "rbx")
-     (ruby-compilation-executable . "jruby")))
+   '((whitespace-line-column . 80)))
  '(vc-annotate-background "#fafafa")
  '(vc-annotate-color-map
    (list
@@ -43,7 +34,7 @@
     (cons 320 "#ae8d8d")
     (cons 340 "#383a42")
     (cons 360 "#383a42")))
- '(vc-annotate-very-old-color nil))y
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
