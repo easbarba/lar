1c8f289c4306c557a51b96edc77a60343a140688
easbarbosa <easbarbosa@pm.me>
2021-02-08 17:52:51 -0300

    mx/erc: add lua, rm nixos,haskell

--- .config/emacs/lisp/init-builtin.el
+++ .config/emacs/lisp/init-builtin.el
@@ -473,8 +473,7 @@
 		   erc-interpret-mirc-color t
 		   erc-spelling-dictionaries '(("#emacs" "american"))
 		   erc-autojoin-channels-alist '(("freenode.net"
-						  "#haskell"
-						  "#nixos"
+						  "#lua"
 						  "#ruby"
 						  "#emacs"))
 		   erc-lurker-hide-list '("JOIN" "PART" "QUIT")
