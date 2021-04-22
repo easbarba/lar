14d1b305f17a2b9860e53ab7315ac8606aecf200
EAS Barbosa <easbarbosa@debian>
2021-04-22 00:00:57 -0300

    add devilspie2

---
+++ .config/devilspie2/workspace_apps.lua
@@ -0,0 +1,24 @@
+if (get_application_name() == "Emacs") then
+   set_window_workspace(1)
+   maximize()
+end
+if (get_application_name() == "Atril Document Viewer") then
+   set_window_workspace(2)
+   maximize()
+end
+if (get_application_name() == "Terminal") then
+   set_window_workspace(3)
+   maximize()
+end
+if (get_application_name() == "Firefox") then
+   set_window_workspace(4)
+   maximize()
+end
+if (get_application_name() == "TelegramDesktop") then
+   set_window_workspace(5)
+   maximize()
+end
+if (get_application_name() == "Pragha Music Player") then
+   set_window_workspace(5)
+   maximize()
+end
