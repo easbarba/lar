501471c5328f20bca3b9e4b749685fd39f0dea87
easbarbosa <easbarbosa@tutanota.me>
2021-05-06 02:20:07 -0300

    nixpkgs: add chromium and bagaceira

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,14 +1,31 @@
-{
+{ pkgs }:
+let
+  inherit (pkgs) lib buildEnv;
+
+in {
   # * nix-env
   allowUnfree = true;
 
   # * Browsers
-  #chromium = {
-   # enablePepperFlash = true;
-  #};
+  chromium = {
+    enableWideVine = true; # enable the DRM to allow things like Netflix to work
+  };
 
   firefox = {
     enableGoogleTalkPlugin = true;
     enableAdobeFlash = true;
+  };
+
+  packageOverrides = pkgs: {
+    desktopDev = lib.lowPrio (buildEnv {
+      name = "bagaceira";
+      paths = with pkgs; [
+        ruby
+        solargraph
+        libyaml
+        elixir
+        nodejs
+      ];
+    });
   };
 }
