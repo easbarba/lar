6dba1d3670484b7d8f5032a4494883f3d5c1c78f
easbarbosa <easbarbosa@tutanota.me>
2021-06-06 16:21:11 -0300

    domm/packs: clean up

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -1,21 +1,19 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here and run 'doom sync'
-;; on the command line, then restart Emacs for the changes to take effect -- or
-;; use 'M-x doom/reload'.
+;; REPOS
 
-(package! matrix-client
-  :recipe (:host github :repo "alphapapa/matrix-client.el"))
+(package! org-make-toc)
+(package! alert)
+(package! duplicate-thing)
+(package! outshine)
+(package! define-word)
+(package! guix)
+(package! zoom)
+(package! bufler)
 
-;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
+;; RECIPE
 
-;; To install a package directly from a remote git repo, you must specify a
-;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
-;; https://github.com/raxod502/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
