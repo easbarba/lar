1b0f368890a555d60d5dedcd2775facb55ca89bf
easbarbosa <easbarbosa@tutanota.me>
2021-06-06 16:21:33 -0300

    doom/config: miramare default theme

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -4,19 +4,17 @@
       user-mail-address "easbarbosa@tutanota.com")
 
 (cond ((member "Hack" (font-family-list))
-       (setq doom-font (font-spec :family "Hack" :size 22 :weight 'semi-light)
-             doom-variable-pitch-font (font-spec :family "sans" :size 22)))
+       (setq doom-font (font-spec :family "Hack" :size 23 :weight 'semi-light)
+             doom-variable-pitch-font (font-spec :family "sans" :size 23)))
       ((member "DejaVu Sans Mono" (font-family-list))
        (setq doom-font (font-spec :family "DejaVu Sans Mono" :size 23 :weight 'semi-light)
              doom-variable-pitch-font (font-spec :family "sans" :size 23))))
 
-(setq doom-theme 'doom-manegarm
+(setq doom-theme 'doom-miramare
       org-directory "~/Documents/"
       display-line-numbers-type t)
 
 ;; BUILT-IN
-
-(use-package! matrix-client)
 
 (load! "lisp/init-dired")
 (load! "lisp/init-utils")
@@ -30,3 +28,4 @@
 
 (load! "lisp/init-repository-languages")
 (load! "lisp/init-repository-misc")
+
