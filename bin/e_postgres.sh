b8de1210206c7f0d7d59d6b4a6dc6b75f4ddabbe
easbarbosa <easbarbosa@tutanota.me>
2021-07-04 21:51:51 -0300

    up

--- bin/e_postgres.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-sudo su - postgres -c "createuser kylo"
-sudo su - postgres -c "createdb kylodb"
-
-sudo -u postgres psql
-
-# GRANT ALL PRIVILEGES ON DATABASE kylodb TO kylo;
