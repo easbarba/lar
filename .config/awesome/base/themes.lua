7c581dccf77d4a6f2f85b010ba01eea498eb6e7f
easbarbosa <easbarbosa@tutanota.me>
2021-07-28 14:27:27 -0300

    awesome: new wallpaper

--- .config/awesome/base/themes.lua
+++ .config/awesome/base/themes.lua
@@ -4,7 +4,6 @@ local beautiful = require("beautiful")
 -- Themes define colours, icons, font and wallpapers.
 
 beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
--- beautiful.init(gears.filesystem.get_configuration_dir() .. "myTheme.lua")
 
 -- Wallpaper
-beautiful.wallpaper = os.getenv("HOME") .. "/Pictures/wallpapers/communist-flag.jpg"
+beautiful.wallpaper = os.getenv("HOME") .. "/Pictures/wallpapers/seo-hyun-jin-1.jpg"
