b28efdf27dd4074066b556166598e3e54432188e
easbarbosa <easbarbosa@tutanota.me>
2021-08-06 18:51:10 -0300

    awesome: use default theme

--- .config/awesome/base/themes.lua
+++ .config/awesome/base/themes.lua
@@ -3,7 +3,7 @@ local beautiful = require("beautiful")
 
 -- Themes define colours, icons, font and wallpapers.
 
-beautiful.init(gears.filesystem.get_themes_dir() .. "theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "theme.lua")
 
 -- Wallpaper
 -- beautiful.wallpaper = os.getenv("HOME") .. "/Pictures/wallpapers/seo-hyun-jin-1.jpg"
