9264c412bb6e105caa7e243376331e96e6f971b9
easbarba <easbarba@protonmail.com>
2022-01-01 06:15:27 -0300

    doom/erc: add debian & debian-ruby

--- .config/doom/lisp/init-builtin.el
+++ .config/doom/lisp/init-builtin.el
@@ -28,6 +28,8 @@
 	erc-spelling-dictionaries '(("#emacs" "american"))
 	erc-autojoin-channels-alist '(("libera.chat"
 				       "#ruby"
+                                       "#debian-ruby"
+                                       "#debian"
                                        "#guix"
 				       "#emacs"))
 	erc-lurker-hide-list '("JOIN" "PART" "QUIT")
