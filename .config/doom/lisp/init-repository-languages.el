31fd71cfaa402d3c2ba1edeb897f9deba0448f85
easbarba <easbarba@outlook.com>
2022-07-23 17:10:55 -0300

    doom/langs: limit lsp to worthy langs

--- .config/doom/lisp/init-repository-languages.el
+++ .config/doom/lisp/init-repository-languages.el
@@ -4,15 +4,6 @@
 ;; ADDITIONAL LANGUAGES TOOLINGS
 ;; ================================
 
-(add-hook! ruby-mode lsp!)
-(add-hook! python-mode lsp!)
-(add-hook! js-mode lsp!)
-(add-hook! css-mode lsp!)
-;; (add-hook! html-mode lsp!)
 (add-hook! shell-script-mode lsp!)
-(add-hook! shell-mode lsp!)
-(add-hook! json-mode lsp!)
-(add-hook! yaml-mode lsp!)
-;; (add-hook! vue-mode-hook lsp!)
 
 (provide 'init-repository-languages)
