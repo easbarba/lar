7d3f4c221ef49a354b1f26805e08299385da1f49
dynab <easbarba@outlook.com>
2022-07-25 13:53:26 -0300

    bin/dots.go: shorter naming

--- .local/bin/s-tools-dots.go
+++ .local/bin/s-tools-dots.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	root := flag.String("deploy", "", "deploy dotfiles links")
+	force := flag.Bool("force", false, "force redeployment of dotfiles links")
 	flag.Parse()
 
 	if *root == "" {
@@ -18,19 +19,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	ignored_list, err := ioutil.ReadFile(filepath.Join(*root, ".dotsignore"))
+	ignore, err := ioutil.ReadFile(filepath.Join(*root, ".dotsignore"))
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	normalized_ignored_list := strings.Split(string(ignored_list), "\n")
-	normalized_root := filepath.Clean(*root)
-	crawler(normalized_root, normalized_ignored_list)
+	fixed_ignored := strings.Split(string(ignore), "\n")
+	fixed_root := filepath.Clean(*root)
+	crawler(fixed_root, fixed_ignored, *force)
 }
 
-func crawler(root string, ignored []string) {
+func crawler(root string, ignored []string, force bool) {
 	filepath.Walk(root,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
@@ -39,33 +40,36 @@ func crawler(root string, ignored []string) {
 			}
 
 			// check if it is to ignore file
-			if ignore_any(root, ignored) {
+			if filterOut(root, ignored) {
 				return nil
 			}
 
-			// finally, print file to be linked
-			fmt.Println(path)
-
+			linkFile(path, force)
 			return nil
 		})
 
-	fmt.Print("root: ", root)
+	println("")
+	fmt.Println("Ignored: ", ignored)
+	fmt.Print("Root: ", root)
 }
 
 // ignore file if its is in .dotsignored
-func ignore_any(root string, ignored []string) bool {
+func filterOut(root string, ignored []string) bool {
 	for _, item := range ignored {
-		// empty string
-		if item == "" {
-			return true
-		}
-
 		rooted_item := filepath.Join(root, item)
-		fmt.Println(rooted_item)
-		if strings.Contains(root, rooted_item) {
+		if item == "" || strings.HasPrefix(root, rooted_item) {
 			return true
 		}
 	}
 
 	return false
+}
+
+func linkFile(path string, force bool) {
+	if force {
+		fmt.Print("Forcing re-linking")
+	}
+
+	// finally, print file to be linked
+	fmt.Println(path)
 }
