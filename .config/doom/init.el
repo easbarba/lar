6ef38a4f0fa786c8a20a80ee7090fe24b02df80c
easbarba <easbarba@outlook.com>
2022-08-24 22:51:51 -0300

    doom/init: cool langs, only!

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -120,11 +120,11 @@
        ;; common-lisp      ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       ;;csharp            ; unity, .NET, and mono shenanigans
+       ;; csharp            ; unity, .NET, and mono shenanigans
        data                ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
-       (elixir +lsp +tree-sitter)    ; erlang done right
+       ;; (elixir +lsp +tree-sitter)    ; erlang done right
        ;;elm               ; care for a cup of TEA?
        emacs-lisp          ; drown in parentheses
        ;; (erlang +lsp)    ; an elegant language for a more civilized
@@ -136,7 +136,7 @@
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       (go +lsp)           ; the hipster dialect
+       ;; (go +lsp +tree-sitter)   ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
        ;; (haskell +lsp)   ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
@@ -158,17 +158,17 @@
        ;; (php +lsp)       ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp)       ; beautiful is better than ugly
+       (python +lsp +tree-sitter)       ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;; racket           ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        rst                 ; ReST in peace
-       (ruby +lsp)         ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (ruby +lsp  +tree-sitter)  ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;; (rust +lsp  +tree-sitter)  ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        (scheme +guile)     ; a fully conniving family of lisps
-       (sh +fish +lsp)     ; she sells {ba,z,fi}sh shells on the C xor
+       (sh +lsp +tree-sitter)  ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
