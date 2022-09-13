4e1653e5ce8a6c603c06d491819de8fbdb21ef92
easbarba <easbarba@outlook.com>
2022-09-13 13:12:22 -0300

    emacs/langs: do not auto format java mode

--- .config/doom/lisp/init-repository-languages.el
+++ .config/doom/lisp/init-repository-languages.el
@@ -4,21 +4,25 @@
 ;; ADDITIONAL LANGUAGES TOOLINGS
 ;; ================================
 
-  (set-ligatures! '(java-mode)
-    ;; Functional
-    ;; :def "void "
-    ;; Types
-    :null "null"
-    :true "true" :false "false"
-    :int "int" :float "float"
-    ;; :str "std::string"
-    :bool "bool"
-    ;; Flow
-    :not "!"
-    :and "&&" :or "||"
-    :for "for"
-    :return "return"
-    :yield "import")
+(setq +format-on-save-enabled-modes
+      '(not emacs-lisp-mode  ; elisp's mechanisms are good enough
+            java-mode))
+
+(set-ligatures! '(java-mode)
+  ;; Functional
+  ;; :def "void "
+  ;; Types
+  :null "null"
+  :true "true" :false "false"
+  :int "int" :float "float"
+  ;; :str "std::string"
+  :bool "bool"
+  ;; Flow
+  :not "!"
+  :and "&&" :or "||"
+  :for "for"
+  :return "return"
+  :yield "import")
 
 
 (provide 'init-repository-languages)
