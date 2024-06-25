befc4c3091ac6d2aaf80905ee07667bb334c2afe
easbarba <easbarba@outlook.com>
2024-06-24 22:46:28 -0300

    bash/functions: +s_binary_decimal

--- .config/bash/functions.bash
+++ .config/bash/functions.bash
@@ -2,12 +2,23 @@ s_mkc() {
     mkdir "$1" && cd "$1" || echo "Could not create/enter folder!"
 }
 
+
+s_binary_decimal() {
+    # desc: from binary to decimal
+    echo $((2#$1))
+}
+
+
 s_decimal_binary() {
+    # desc: from to decimal to binary
+
     local decimal=$1
-    local bin=""
-    while [ $decimal -gt 0 ]; do
-        bin=$(( decimal % 2 ))$bin
+    local binary=""
+
+    while [ "$decimal" -gt 0 ]; do
+        binary=$(( decimal % 2 ))$binary
         decimal=$(( decimal / 2 ))
     done
-    echo $bin
+
+    echo $binary
 }
